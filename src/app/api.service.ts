7ea820d42e57cbe644c9f7b1f4094bc5e5cc4dc1
Felipe Lima <flf.felipe@gmail.com>
2017-11-02 23:24:38 -0200

    Create REST methods stubs

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { environment } from './../environments/environment';
+import { Todo } from './todo';
 import { Http } from '@angular/http';
 
 const API_URL = environment.apiUrl;
@@ -9,4 +10,28 @@ export class ApiService {
 
   constructor(private http: Http) { }
 
+  // API: GET /todos
+  public getAllTodos() {
+    // will use this.http.get()
+  }
+
+  // API: POST /todos
+  public createTodo(todo: Todo) {
+    // will use this.http.post()
+  }
+
+  // API: GET /todos/:id
+  public getTodoById(todoId: number) {
+    // will use this.http.get()
+  }
+
+  // API: PUT /todos/:id
+  public updateTodo(todo: Todo) {
+    // will use this.http.put()
+  }
+
+  // DELETE /todos/:id
+  public deleteTodoById(todoId: number) {
+    // will use this.http.delete()
+  }
 }
